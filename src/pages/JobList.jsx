678f2bed2beec34cbdc7a1492a1d9cd38caf72e3
RNyaupane <roshan.nyaupane@apexcollege.edu.np>
2023-07-18 06:42:12 +0545

    jobList edited

--- src/pages/JobList.jsx
+++ src/pages/JobList.jsx
@@ -15,6 +15,9 @@ const JobList = () => {
 
   const jobPosts = useSelector((state) => state.acs.jobPosts);
 
+  console.log(jobPosts);
+  //fetching jobType from store
+  const jobType = useSelector((state) => state.acs.jobPosts);
   useEffect(() => {
     dispatch(fetchJobPosts());
     setLoading(false);
@@ -37,21 +40,50 @@ const JobList = () => {
     return <LoadingSpinner />;
   } else {
     return (
-      <div>
-        {jobPosts.map((job) => (
-          <JobItem
-            key={job.id}
-            id={job.id}
-            company={job.company_name}
-            title={job.title}
-            location={job.location}
-            job_type={job.job_type}
-            salary={job.salary}
-            expire_date={getFormattedDate(job.expire_date)}
-            applyNow={() => handleApplyNow(job.id)}
-          />
-        ))}
-      </div>
+      <section id="special" className="py-5">
+        <div className="container">
+          <div className="title text-center py-5">
+            <h2 className="position-relative d-inline-block">Available Jobs</h2>
+          </div>
+
+          <div className="special-list row g-0">
+            <div className="d-block justify-content-center text-center mb-4">
+              <div className=" flex-wrap justify-content-center my-3 filter-button-group d-inline">
+                <button type="button" className="btn m-2  active-filter-btn" data-filter="*">All</button>
+                <button type="button" className="btn m-2 " >Remote</button>
+                <button type="button" className="btn m-2 " >Hybrid</button>
+                <button type="button" className="btn m-2 " >Onsite</button>
+              </div>
+              <select defaultValue={"default"} className="form-select form-select-lg border shadow-none mb-3 w-auto fs-6 end-0 d-inline float-end" aria-label=".form-select-lg example">
+                <option value="default" disabled>Job Type </option>
+                <option value="1">Internship</option>
+                <option value="2">Entry Level</option>
+                <option value="3">Mid Level</option>
+                <option value="3">Experienced</option>
+              </select>
+            </div>
+            <div className="tab-content">
+              <div id="tab-1" className="tab-pane fade show p-0 active">
+                <div>
+                  {jobPosts.map((job) => (
+                    <JobItem
+                      key={job.id}
+                      id={job.id}
+                      company={job.company_name}
+                      title={job.title}
+                      location={job.location}
+                      job_type={job.job_type}
+                      salary={job.salary}
+                      expire_date={getFormattedDate(job.expire_date)}
+                      applyNow={() => handleApplyNow(job.id)}
+                    />
+                  ))}
+                </div>
+              </div>
+            </div>
+          </div>
+        </div>
+      </section>
     );
   }
 };
