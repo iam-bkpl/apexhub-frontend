0fa6ba72c3abf77bf04575755e0d0515dbc3799b
iam-bkpl <kapil.bhandari@apexcollege.edu.np>
2023-07-19 20:52:44 +0545

    change class name

--- src/containers/Testimonials.jsx
+++ src/containers/Testimonials.jsx
@@ -4,21 +4,21 @@ import iambkpl from "../assets/defaults/iambkpl.jpg";
 const Testimonials = () => {
   return (
     <>
-      <section class="team section-bg">
-        <div class="title text-center pt-3 pb-5">
-          <h2 class="position-relative d-inline-block ms-4">Our Client Says</h2>
+      <section className="team section-bg">
+        <div className="title text-center pt-3 pb-5">
+          <h2 className="position-relative d-inline-block ms-4">Our Client Says</h2>    
         </div>
         <div
           id="carouselExampleIndicators"
-          class="carousel slide"
+        className="carousel slide"
           data-bs-ride="carousel"
         >
-          <div class="carousel-indicators ">
+          <div className="carousel-indicators ">
             <button
               type="button"
               data-bs-target="#carouselExampleIndicators"
               data-bs-slide-to="0"
-              class="active bg-secondary"
+              className="active bg-secondary"
               aria-current="true"
               aria-label="Slide 1"
             ></button>
@@ -27,31 +27,31 @@ const Testimonials = () => {
               data-bs-target="#carouselExampleIndicators"
               data-bs-slide-to="1"
               aria-label="Slide 2"
-              class="bg-secondary"
+              className="bg-secondary"
             ></button>
             <button
               type="button"
               data-bs-target="#carouselExampleIndicators"
               data-bs-slide-to="2"
               aria-label="Slide 3"
-              class="bg-secondary"
+              className="bg-secondary"
             ></button>
           </div>
-          <div class="carousel-inner w-auto px-5 ms-5">
-            <div class="carousel-item active" data-bs-interval="5000">
-              <div class="row mb-5">
-                <div class="col-lg-3 col-sm-6 col-md-5 d-flex align-items-stretch mb-2">
-                  <div class="member">
-                    <div class="member-img">
+          <div className="carousel-inner w-auto px-5 ms-5">
+            <div className="carousel-item active" data-bs-interval="5000">
+              <div className="row mb-5">
+                <div className="col-lg-3 col-sm-6 col-md-5 d-flex align-items-stretch mb-2">
+                  <div className="member">
+                    <div className="member-img">
                       <img
                         src={iambkpl}
-                        class="img-fluid rounded-pill"
+                        className="img-fluid rounded-pill"
                         alt=""
                       />
                     </div>
-                    <div class="member-info px-3 my-4  text-center">
+                    <div className="member-info px-3 my-4  text-center">
                       <h4>Client 4</h4>
-                      <small class="text-secondary">
+                      <small className="text-secondary">
                         One aspect that sets APEX-HUB apart from others is the
                         exceptional customer support.
                       </small>
@@ -59,53 +59,53 @@ const Testimonials = () => {
                   </div>
                 </div>
 
-                <div class="col-lg-3 col-sm-6 col-md-5 d-flex align-items-stretch mb-2">
-                  <div class="member">
-                    <div class="member-img">
+                <div className="col-lg-3 col-sm-6 col-md-5 d-flex align-items-stretch mb-2">
+                  <div className="member">
+                    <div className="member-img">
                       <img
                         src={iambkpl}
-                        class="img-fluid rounded-pill"
+                        className="img-fluid rounded-pill"
                         alt=""
                       />
                     </div>
-                    <div class="member-info px-3 my-4 text-center">
+                    <div className="member-info px-3 my-4 text-center">
                       <h4>Client 3</h4>
-                      <span class="text-secondary">Description </span>
+                      <span className="text-secondary">Description </span>
                     </div>
                   </div>
                 </div>
               </div>
             </div>
-            <div class="carousel-item" data-bs-interval="5000">
-              <div class="row mb-5">
-                <div class="col-lg-3 col-sm-6 col-md-5 d-flex align-items-stretch mb-2 ">
-                  <div class="member">
-                    <div class="member-img">
+            <div className="carousel-item" data-bs-interval="5000">
+              <div className="row mb-5">
+                <div className="col-lg-3 col-sm-6 col-md-5 d-flex align-items-stretch mb-2 ">
+                  <div className="member">
+                    <div className="member-img">
                       <img
                         src={iambkpl}
-                        class="img-fluid rounded-pill"
+                        className="img-fluid rounded-pill"
                         alt=""
                       />
                     </div>
-                    <div class="member-info px-3 my-4 text-center">
+                    <div className="member-info px-3 my-4 text-center">
                       <h4>Client 2</h4>
-                      <span class="text-secondary">Description</span>
+                      <span className="text-secondary">Description</span>
                     </div>
                   </div>
                 </div>
 
-                <div class="col-lg-3 col-sm-6 col-md-5 d-flex align-items-stretch mb-2 ">
-                  <div class="member">
-                    <div class="member-img">
+                <div className="col-lg-3 col-sm-6 col-md-5 d-flex align-items-stretch mb-2 ">
+                  <div className="member">
+                    <div className="member-img">
                       <img
                         src={iambkpl}
-                        class="img-fluid rounded-pill"
+                        className="img-fluid rounded-pill"
                         alt=""
                       />
                     </div>
-                    <div class="member-info px-3 my-4 text-center">
+                    <div className="member-info px-3 my-4 text-center">
                       <h4>Client 2</h4>
-                      <span class="text-secondary">
+                      <span className="text-secondary">
                         Description that sets APEX-HUB apart from others is the
                         exceptional customer support.
                       </span>
