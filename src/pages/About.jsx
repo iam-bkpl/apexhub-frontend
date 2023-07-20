d63d836cbe7078a3cb02e609703f5dd0fa500f61
RNyaupane <roshan.nyaupane@apexcollege.edu.np>
2023-07-20 21:32:25 +0545

    header added to about us

--- src/pages/About.jsx
+++ src/pages/About.jsx
@@ -1,10 +1,12 @@
 import React from 'react'
 import AboutUs from '../containers/AboutUs'
 import Team from '../containers/Team'
+import Header from '../containers/Header'
 
 const About = () => {
   return (
     <>
+            <Header/>
             <AboutUs/>
             <Team/>
     </>
